55fbaf67b72a6dae1c98a1cd74c951a0eec35efb
Lebogang Mokoena <ditswe24@gmail.com>
2026-05-24 17:05:50 +0200

    Update GenreService.js by adding GenreService module to resolve genre IDs to title strings

--- src/utils/GenreService.js
+++ src/utils/GenreService.js
@@ -1,1 +1,19 @@
+import { genres } from "../data.js";
 
+/**
+ * Genre Service - Maps genre IDs to human-readable genre names.
+ *
+ * @principle SRP - Only responsible for resolving genre IDs to titles.
+ */
+export const GenreService = {
+  /**
+   * Resolves an array of genre IDs into an array of genre title strings.
+   * @param {number[]} genreIds - Array of genre IDs.
+   * @returns {string[]} Array of genre titles.
+   */
+  getNames(genreIds) {
+    return genreIds.map(
+      (id) => genres.find((g) => g.id === id)?.title || "Unknown"
+    );
+  },
+};
